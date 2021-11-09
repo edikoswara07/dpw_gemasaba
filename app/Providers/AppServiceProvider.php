4ddea0ffa597afe07613fd04bfce4d2059181564
edikoswara07 <edikoswara1st@gmail.com>
2021-11-10 02:18:31 +0700

    kernel v2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace App\Providers;
 
+
+
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +27,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // condition 1
+        if (env('APP_ENV') === 'production') {
+            URL::forceSchema('https');
+        }
+
+        // condition 2
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
+
+        // condition 3
+        if (env('APP_FORCE_HTTPS', false)) {
+            URL::forceScheme('https');
+        }
     }
 }
